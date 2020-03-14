aea9468115b6d5d2a110dd104066381f3bc535fd
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-03-14 10:29:28 +0100

    updated package to 20200313-8751 version

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,13 +2,13 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . "$(Join-Path $toolsDir commonEnv.ps1)"
 
-$url = 'https://dukeworld.com/eduke32/synthesis/20190919-8133/eduke32_win64_20190919-8133.7z'
+$url = 'https://dukeworld.com/eduke32/synthesis/20200313-8751/eduke32_win64_20200313-8751.7z'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installLocation
   url           = $url
   softwareName  = 'eduke32*'
-  checksum      = '97062BE841210377B31F0C016CD20604F9C7F6329855D19E6EAC71C728CB8F3C'
+  checksum      = 'AB762D5A9C8AED54A107E4B79F9E33D527418E9F621A0BEB1266F57F3F60AE28'
   checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
