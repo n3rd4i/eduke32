655deba8020af56faa9fae6655566bfa464b96c4
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-05-25 22:29:05 +0200

    updated to ver. 2020.05.25.9041 and added TSL 1.2 support

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,13 +2,12 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . "$(Join-Path $toolsDir commonEnv.ps1)"
 
-$url = 'https://dukeworld.com/eduke32/synthesis/20200313-8751/eduke32_win64_20200313-8751.7z'
+$url = 'https://dukeworld.com/eduke32/synthesis/20200525-9041-249a40a1d/eduke32_win64_20200525-9041-249a40a1d.7z'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installLocation
   url           = $url
-  softwareName  = 'eduke32*'
-  checksum      = 'AB762D5A9C8AED54A107E4B79F9E33D527418E9F621A0BEB1266F57F3F60AE28'
+  checksum      = 'E270364886272F2D998AD2915D9537C84E8C152E6E8DD7A1215E5437AB6A6A45'
   checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
@@ -17,8 +16,3 @@ Install-ChocolateyZipPackage @packageArgs
 Install-ChocolateyShortcut -ShortcutFilePath "$(Join-Path $startMenuDir $GameName).lnk" `
   -TargetPath "$binPath" `
   -WorkingDirectory "$installLocation"
-
-## Desktop
-# Install-ChocolateyShortcut -ShortcutFilePath "$shortcutPath" `
-#   -TargetPath "$binPath" `
-#   -WorkingDirectory "$installLocation"
