7d70c8d720400e79f4408ff49bc7b537361cea7a
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-06-09 22:33:47 +0200

    first updated working version

---
+++ update.ps1
@@ -0,0 +1,37 @@
+import-module au
+
+$domain   = 'https://dukeworld.com'
+$releases = "$domain/eduke32/synthesis"
+
+function global:au_SearchReplace {
+  @{ 
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"   	= "`$1'$($Latest.Checksum32)'"
+    }
+  }
+}
+
+# url           = 'https://dukeworld.com/eduke32/synthesis/20200525-9041-249a40a1d/eduke32_win64_20200525-9041-249a40a1d.7z'
+function global:au_GetLatest {
+  $folders_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex = '\d{4}\d{2}\d{2}-\d{4}-\w{9}' # hash included version
+  $folder = $folders_page.links | ? href -match $regex | select -First 1 -expand href
+  $download_page = Invoke-WebRequest -Uri "$releases/$folder" -UseBasicParsing
+  $regex = '.7z$'
+  $url_file = $download_page.links | ? href -match $regex | select -First 1 -expand href
+  $url = ("$releases/$folder", $url_file)  -join ''
+
+  $token = $url_file -split 'eduke32_win64_' | select -First 1 -Skip 1
+  $raw_version = $token -split '.7z' | select -Last 1 -Skip 1
+  $tokens = $raw_version -split '-' | select -First 2
+  $semantic_ver = [regex]::split($tokens[0], '(\d{4})(\d{2})(\d{2})') | Where-Object { $_ }
+  $version_pre = $semantic_ver -join '.'
+  $version = ($version_pre, $tokens[1]) -join '.'
+  return @{ Version = $version; URL32 = $url }
+}
+
+update -ChecksumFor 32
+
+# Reference:
+# https://github.com/majkinetor/au/blob/master/README.md#creating-the-package-updater-script
