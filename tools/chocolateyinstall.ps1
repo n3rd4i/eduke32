d110a5b2253c1e79bd96938ea6527c194293daea
Andrei Hutanu <andrei.hutanu@evalan.com>
2021-03-06 13:43:09 +0100

    updated to 2021.03.04.9317

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installLocation
-  url           = 'https://dukeworld.com/eduke32/synthesis/20210221-9314-a2d4c2794/eduke32_win64_20210221-9314-a2d4c2794.7z'
-  checksum      = 'ff87d761810c1d9501d7e9c76cca638da841961238ff1283dc0ce8af96da3721'
+  url           = 'https://dukeworld.com/eduke32/synthesis/20210304-9317-fe2bcb7b5/eduke32_win64_20210304-9317-fe2bcb7b5.7z'
+  checksum      = 'a5717a7491e0489ee9439d3af98f6593ac7a0deb56449e2f964c70821dd343c4'
   checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
